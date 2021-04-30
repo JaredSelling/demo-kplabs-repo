8697c665559b50ee65bb3bda20b3c35c39808c90
JaredSelling <selling.jared@gmail.com>
2021-04-30 07:16:24 -0400

    add dev ec2

--- ec2.tf
+++ ec2.tf
@@ -2,3 +2,9 @@ resource "aws_instance" "myec2" {
     ami = "ami-0742b4e673072066f"
     instance_type = "t2.micro"
 }
+
+resource "aws_instance" "dev_ec2" {
+  ami = "ami-001628438d5d7d524"
+  instance_type = "t2.micro"
+  provider = "aws.dev"
+}
